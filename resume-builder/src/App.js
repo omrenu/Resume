6ff25ab105ccacdcf8240fb7e259d9b56ead9edd
om <rdmahesh22@gmail.com>
2023-05-20 20:50:53 +0530

    added basic styles

--- resume-builder/src/App.js
+++ resume-builder/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 function App() {
   return (
     <React.Fragment>
       <Header />
       <Main />
+      <Footer />
     </React.Fragment>
   );
 }
