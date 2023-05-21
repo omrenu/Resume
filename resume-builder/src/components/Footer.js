aaa7f4826e1fa4f9c44399b74126db0054220494
om <rdmahesh22@gmail.com>
2023-05-21 16:56:53 +0530

    Finish Footer

--- resume-builder/src/components/Footer.js
+++ resume-builder/src/components/Footer.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import styled from "styled-components";
+import { FaGithub } from "react-icons/fa";
 const Footer = () => {
+    const StyleFooter = styled.footer`
+        display:flex;
+        align-items:center;
+        padding: 2rem;
+        background-color:${({ theme }) => theme.colors.dark};
+        color: ${({ theme }) => theme.colors.light};
+    `;
+    const Copyright = styled.p`
+     margin-right: 1rem;
+    `;
+    const Link = styled.a`
+        top: 0.2rem;
+        color: ${({ theme }) => theme.colors.light};
+        &:hover {
+        color: ${({ theme }) => theme.colors.lightHover};
+        }
+    `;
+
     return (
-        <div></div>
+        <StyleFooter>
+            <Copyright>Copyright © 2021 rdmahesh22 </Copyright>
+            <Link href="https://github.com/omrenu" target="_blank">
+                <FaGithub />
+            </Link>
+        </StyleFooter>
     );
 }
 export default Footer;
